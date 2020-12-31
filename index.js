e06b37bf0a6942cae7af07cd89fadc91766a9891
nano235 <revivaladolor@yahoo.com>
2020-12-31 12:47:55 +0100

    playing with Objects

---
+++ index.js
@@ -0,0 +1,17 @@
+const firstObject = {
+    school: "uniben",
+    name: "nano",
+    friends: {
+        one: "Ghenero",
+        two: "Onyi",
+        three: "Philip"
+    }
+}
+
+firstObject.friends.four = "Aise"
+
+console.log(firstObject)
+
+firstObject.friends.four = "Dom"
+
+console.log(firstObject)
