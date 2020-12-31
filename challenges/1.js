bfa74b2087965ae43e2f0b1c62cfd3c4d1077a50
nano235 <revivaladolor@yahoo.com>
2020-12-31 13:05:31 +0100

    added challenges folder

---
+++ challenges/1.js
@@ -0,0 +1,15 @@
+let myPost = {}
+
+myPost.postTitle = {}
+
+myPost.postLikes = 0
+
+myPost.shared = false
+
+myPost.postLikes = 1
+
+console.log(myPost)
+
+delete myPost.shared
+
+console.log(myPost)
