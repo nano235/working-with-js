ef3bd27da7cd82aa96bb4433436e5a386f63e798
DESKTOP-AMLSC2G\FRANCA <revivalnano@gmail.com>
2021-01-05 22:50:20 +0100

    playing with operators

---
+++ challenges/4.js
@@ -0,0 +1,11 @@
+let myVariable1, myVariable2
+
+myVariable2 = 5
+myVariable1 = 5
+
+myVariable1 = Number(myVariable1)
+myVariable2 = Number(myVariable2)
+
+if (myVariable1 == myVariable2){
+    console.log("true")
+}
