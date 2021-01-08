ffe1f61acb480be8425d3d86b4b9da00ec39fdb3
DESKTOP-AMLSC2G\FRANCA <revivalnano@gmail.com>
2021-01-08 21:47:24 +0100

    using unary plus to change variable type to number

--- challenges/4.js
+++ challenges/4.js
@@ -3,9 +3,6 @@ let myVariable1, myVariable2
 myVariable2 = 5
 myVariable1 = 5
 
-myVariable1 = Number(myVariable1)
-myVariable2 = Number(myVariable2)
-
-if (myVariable1 == myVariable2){
+if (+myVariable1 == +myVariable2){
     console.log("true")
 }
