170021ed0dd3d8435e82a20b71a588675c5cf910
DESKTOP-AMLSC2G\FRANCA <revivalnano@gmail.com>
2021-01-21 04:09:33 +0100

    fixed the error of not defining variables

--- challenges/7.js
+++ challenges/7.js
@@ -1,7 +1,9 @@
-for (g = 0; j<arrayOfCars.length; g++){
+let g
+for (g = 0; g<arrayOfCars.length; g++){
     console.log(arrayOfCars[g])
 }
 
+let j
 j = 0
 while (j < arrayOfCars.length){
     console.log(arrayOfCars[j])
