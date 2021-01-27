ecfff789fde3fcb67eee5d4d63c301e0084ca4b5
nano235 <revivaladolor@yahoo.com>
2021-01-27 20:13:58 +0100

    I used another method in comparing the random number to the array. I used functions, for...of and do...while loops

--- challenges/8.js
+++ challenges/8.js
@@ -26,14 +26,44 @@ const myNumbers = [
     8351
 ]
 
-let newRandomNumber = Math.floor(Math.random() * 10000) + 1000 //creates a random number
+let newRandomNumber = Math.floor(Math.random() * 1000) + 1000 //creates a random number
 console.log(newRandomNumber)//checks to see what the random number is
 
 for (let i in myNumbers){//iterates over the array
-    if(newRandomNumber === myNumbers[i]){//compares the random number with each value of the array
-        newRandomNumber = Math.floor(Math.random() * 10000) + 1000//if the random number is equal to any value in the arrya, it creates another
+    if(newRandomNumber === myNumbers){//compares the random number with each value of the array
+        newRandomNumber = Math.floor(Math.random() * 10000) + 1000//if the random number is equal to any value in the array, it creates another
     }
 }
 myNumbers.push(newRandomNumber)//adds the random number to the array if it meets the condition in the for loop
+console.log(myNumbers)
+
+//another way of doing same comparism
+
+let MAX = 9999
+let MIN = 1000
+let randomNumber
+
+function getRandomNumber (max, min){
+    randomNumber = Math.floor(Math.random() * (max - min) + min)
+    return randomNumber
+}
+
+
+function theLoop (randomNumber){
+    for (let i of myNumbers){
+        if (randomNumber === myNumbers){
+            console.log("Hello")
+            return
+        }
+    }
+}
+
+do{
+    randomNumber = getRandomNumber(MAX, MIN)
+}while(
+    theLoop(randomNumber)
+)
+
+myNumbers.push(randomNumber)
 console.log(myNumbers)
 
