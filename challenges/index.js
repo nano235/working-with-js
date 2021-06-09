385eba32620ca677112aa7d978120aff251342c6
nano235 <revivaladolor@yahoo.com>
2021-06-09 23:56:41 +0100

    practice and challenge with forEach loop

--- challenges/index.js
+++ challenges/index.js
@@ -52,3 +52,45 @@ getBtn.addEventListener("click", () => {
 // });
 
 
+
+
+// CHALLENGE
+
+const myCities = ["London", "New York", "Singapore"];
+
+/* Create a function called "arrayInfo" that will return string like 
+"London is located at the index 0 in the myCities array" */
+
+/* Iterate over the "myCities" array, call "arrayInfo" 
+    function for each of the elements and print result of the 
+    call to the console */
+
+// Option 1: "for"
+
+// Option 2: "forEach"
+
+// Solution
+
+const arrayInfo = (element, index) => `${element} is located at index ${index} in the myCities array`;
+// Option 1
+
+for (let i = 0; i < myCities.length; i++) {
+    console.log(arrayInfo(myCities[i], i));
+}
+
+// Option 2
+myCities.forEach ((element, index) =>
+    console.log(arrayInfo(element, index))
+)
+
+
+
+
+
+
+
+
+
+
+
+
