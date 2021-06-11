b6ef27206f310cc815cbe7463148c36bdfc05da1
nano235 <revivaladolor@yahoo.com>
2021-06-11 16:05:48 +0100

    some() and every(), challenge on some() and every()

--- challenges/index.js
+++ challenges/index.js
@@ -191,4 +191,24 @@ console.log(arraysAreEqual(A, D)); // false
 
 
 
+// CHALLENGE
+const transport = ["Bus", "Car", "Bicycle", "Airplane"];
+
+/* Create a function "elementFound" with two parameters "inputArray" and "searchElement".
+If "searchElement" is found in the "inputArray" - return "true".
+Otherwise return "false"
+Hint: Use "some" helper method to solve this
+*/
+
+// Solution
+
+const elementFound = (inputArray, searchElement) => {
+    return inputArray.some(element => element === searchElement)
+}
+
+console.log(elementFound(transport, "Bus")); // true
+console.log(elementFound(transport, "Phone")); // false
+console.log(elementFound(transport, "Airplane")); // true
+
+
 
