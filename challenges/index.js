db86dbdf8cd6585d427050df03d306c6bb5d08af
nano235 <revivaladolor@yahoo.com>
2021-06-12 12:30:14 +0100

    include() and push(), challenge on include() and push()

--- challenges/index.js
+++ challenges/index.js
@@ -247,14 +247,6 @@ const elementIsIncluded = (searchElement, array) =>{
 	return array.map ((element) =>  JSON.stringify(element)).includes(JSON.stringify(searchElement))
 }
 
-
-// tags.forEach ((element, index, array) =>{
-//     console.log(element)
-//     element.forEach(element => {
-//         console.log(element);
-//     })
-// })
-
   
   console.log(elementIsIncluded(["css", "flexbox"], tags)); // true
   
@@ -270,6 +262,36 @@ const elementIsIncluded = (searchElement, array) =>{
   
   console.log(elementIsIncluded(25, primitiveTypesArray)); // true
 
+
+
+//   CHALLENGE
+
+const myNumbers = [123, 50, 27];
+
+/* Create a function "pushIfUnique" with two parameters "inputArray" and "newElement".
+If "inputArray" already contains "newElement" print "{newElement} is already in the array" to the console.
+Otherwise push "newElement" to the "inputArray". 
+NOTE: We assume that "inputArray" may contain only primitive variables types
+*/
+
+// Solution
+
+const pushIfUnique = (inputArray, newElement) => {
+	if(inputArray.includes(newElement)) console.log(`${newElement} is already in the array`)
+	else inputArray.push(newElement)
+}
+
+pushIfUnique(myNumbers, 50); // "50 is already in the array"
+console.log(myNumbers); // [123, 50, 27]
+
+pushIfUnique(myNumbers, 80);
+console.log(myNumbers); // [123, 50, 27, 80]
+
+pushIfUnique(myNumbers, 80); // "80 is already in the array"
+console.log(myNumbers); // [123, 50, 27, 80]
+
+pushIfUnique(myNumbers, 77);
+console.log(myNumbers); // [123, 50, 27, 80, 77]
 
 
 
