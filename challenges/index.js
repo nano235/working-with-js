e82ce349d645fb95e9298d8305195a66ec0c7adc
nano235 <revivaladolor@yahoo.com>
2021-06-14 23:08:15 +0100

    challenges for reduce() and sort()

--- challenges/index.js
+++ challenges/index.js
@@ -323,6 +323,7 @@ const statusUpdate = [
 
 //   Solution
 
+
 const popularPostsIds = (posts, minimalComentsQty) => {
 	return posts.reduce((popularPosts, post) => {
 		if (post.comments >= minimalComentsQty) {
@@ -334,6 +335,232 @@ const popularPostsIds = (posts, minimalComentsQty) => {
   
   console.log(popularPostsIds(statusUpdate, minimalComentsQty)); // [3421, 8135]
 
+
+
+  // Mini self challenge
+
+
+  const tree = {
+    id: "x",
+    special: "special",
+    children: [
+        {
+            id: "y",
+        },
+        {
+            id: "z",
+            ordinary: "special",
+            special: "special",
+            children: [
+                {
+                    id: "m", 
+                    children: [
+                        {
+                            id: "n",
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
+};
+
+const flattenedTree = {
+    x: { id: "x", special: "special", children: ["y", "z"] },
+    y: { id: "y", parentId: "x" },
+    z: {
+        id: "z",
+        children: ["m"],
+        parentId: "x",
+        ordinary: "special",
+        special: "special",
+    },
+    m: { id: "m", children: ["n"], parentId: "z" },
+    n: { id: "n", parentId: "m" },
+};
+
+// Write a function which takes the flattenedTree and finds the nearest ancestor which has a given property
+// return the id of the ancestor which has the given property
+// or return undefined
+
+function findPropertyInTree(startId, treeArray, propertyName){
+	// write your function here
+	// check if startId has a parent;
+	// parentId is present
+	// check if parent has propertyName
+	// if yes return parentId 
+	// if not, new item is parent and check for parent id of new item
+
+	let ancestorId;
+	let parent = treeArray[startId].parentId;
+	// if(typeof parent !== "undefined"){
+	// 	if(typeof treeArray[parent][propertyName] !== "undefined") return parent;
+
+	// 	if(typeof treeArray[parent].parentId !== "undefined"){
+	// 		parent = treeArray[parent].parentId		
+	// 		if(typeof treeArray[parent][propertyName] !== "undefined") {
+	// 		return parent;}
+	// 	}
+	// }
+
+	// return ancestorId;
+
+
+		while (parent) {
+      if (typeof parent === 'undefined')return undefined
+      if(typeof treeArray[parent][propertyName] !== "undefined")return parent
+      parent = treeArray[parent].parentId	
+		}
+}
+
+console.log(findPropertyInTree("m", flattenedTree, "anything")); // undefined
+console.log(findPropertyInTree("y", flattenedTree, "special")); // x
+console.log(findPropertyInTree("n", flattenedTree, "special")); // z
+console.log(findPropertyInTree("n", flattenedTree, "ordinary")); // z
+console.log(findPropertyInTree("z", flattenedTree, "special")); // x
+console.log(findPropertyInTree("x", flattenedTree, "special")); // undefined
+
+
+
+// CHALLENGE
+
+const products = [
+  {
+    title: "Phone case",
+    price: 23,
+    quantity: 2,
+    category: "Accessories"
+  },
+  {
+    title: "Android phone",
+    price: 150,
+    quantity: 1,
+    category: "Phones"
+  },
+  {
+    title: "Headphones",
+    price: 78,
+    quantity: 1,
+    category: "Accessories"
+  },
+  {
+    title: "Sport Watch",
+    price: 55,
+    quantity: 2,
+    category: "Watches"
+  }
+];
+
+
+/* Create a function "quantityByCategories" with one parameter "products".
+This function "quantityByCategories" should return an object with keys equal 
+to categories and values equal to sum of all quantities in each category.
+*/
+
+
+// Solution
+
+const quantityByCategories = products => {
+  return products.reduce((items, cart) => {
+    items[cart.category] = (items[cart.category] || 0) + cart.quantity
+    return items
+  }, {})
+}
+
+
+console.log(quantityByCategories(products));
+/* {
+  Accessories: 3,
+  Phones: 1,
+  Watches: 2
+} */
+
+
+
+// CHALLENGE
+
+const persons = [
+  {
+    name: "Andy",
+    friendsQty: 10,
+    index: 1
+  },
+  {
+    name: "Mike",
+    friendsQty: 5,
+    index: 2
+  },
+  {
+    name: "Sophia",
+    friendsQty: 10,
+    index: 3
+  },
+  {
+    name: "Joshua",
+    friendsQty: 3,
+    index: 4
+  },
+  {
+    name: "John",
+    friendsQty: 10,
+    index: 5
+  },
+  {
+    name: "Gabriella",
+    friendsQty: 8,
+    index: 6
+  },
+  {
+    name: "Tyler",
+    friendsQty: 7,
+    index: 7
+  },
+  {
+    name: "Dylan",
+    friendsQty: 2,
+    index: 8
+  },
+  {
+    name: "Sarah",
+    friendsQty: 5,
+    index: 9
+  },
+  {
+    name: "Alexa",
+    friendsQty: 10,
+    index: 10
+  },
+  {
+    name: "Henry",
+    friendsQty: 10,
+    index: 11
+  },
+  {
+    name: "Arianna",
+    friendsQty: 10,
+    index: 12
+  }
+];
+
+/* Create a function "sortPersonsByFriendsQty" with one parameter "persons".
+This function "sortPersonsByFriendsQty" should sort input array of persons 
+by friendsQty of each person in ascending order and return resulting array.
+*/
+
+// Solution
+
+const sortPersonsByFriendsQty = persons => {
+  persons.sort((a, b) => a.friendsQty - b.friendsQty)
+}
+
+sortPersonsByFriendsQty(persons);
+/* Which sorting algorithm is used here in Google Chrome, Firefox, Node.js? 
+Is this sorting stable or not?
+*/
+
+console.log(persons);
+/* Array of persons sorted by friendsQty in ascending order 
+*/
 
 
 
