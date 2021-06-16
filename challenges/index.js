b741fa3f9ac3a46b3011bf7ba602e490c1fcbcc2
nano235 <revivaladolor@yahoo.com>
2021-06-16 10:38:45 +0100

    challenge for tagged template literal

--- challenges/index.js
+++ challenges/index.js
@@ -563,5 +563,60 @@ console.log(persons);
 */
 
 
+// CHALLENGE
+
+/* Create a function "taggedTemplate" that will return a regular string (not template literal).
+Use appropriate method of the function definition and needed amount of arguments.
+Use all knowledge gained in the previous sections.
+IMPORTANT: Input template literal may have *ANY* quantity of the expressions. 
+NOTE: If you are stuck - hints down below!
+*/
+
+// FIRST test case
+const ab = 10;
+const ba = 5;
+// const sum = taggedTemplate`Sum of the two variables a(${ab}) and b(${ba}) is ${ab +
+//   ba}`;
+
+// console.log(sum);
+/* Sum of the two variables ab(10) and ba(5) is 15 */
+
+// SECOND test case
+const girl = "Alice";
+const boy = "Bob";
+const friendsInfo = taggedTemplate`${girl} and ${boy} are friends!`;
+
+// Solution
+
+function taggedTemplate(strArr, ...keys) {  
+  let arr = [strArr[0]];
+  // keys.forEach((key, i) => {
+  //   arr.push(key, strArr[i + 1])
+  // })
+
+  return keys.reduce((key, n, i) => {
+    key.push(n, strArr[i + 1])
+    return key
+  }, [strArr[0]]).join('');
+
+  // return arr.join("");
+}
+
+console.log(friendsInfo);
+/* Alice and Bob are friends! */
+
+/* HINTS
+1. Use regular function declaration instead of anonymous arrow function expression assigned to the variable.
+In such way you will get access to the "arguments" inside of the function
+2. First argument in the function is array of strings
+3. Other arguments are results of all expressions from the template literal.
+4. Use "Array.from(arguments)" + "slice" array helper method to create an expressions values array. 
+In the first test case this array needs to be [10, 5, 15]
+5. Use "reduce" array helper method in order to construct resulting string that will be returned at the end of the function.
+6. In the callback function for "reduce" helper method you need to concatenate pairs (string, expression value). 
+First pair in the first test case will be ("Sum of the two variables a(", "10").
+*/
+
+
 
 
