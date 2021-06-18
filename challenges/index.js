b4643bf4137c8e7f097f25121c0b179d77e8f6fc
nano235 <revivaladolor@yahoo.com>
2021-06-18 23:55:35 +0100

    challenge for default function parameters

--- challenges/index.js
+++ challenges/index.js
@@ -675,4 +675,46 @@ console.log(meanScore(...scores4)); // Supplied arguments must contain only numb
 
 
 
+// CHALLENGE
+
+/* Create a function "weatherForecast" with two parameters "city" and "weather". 
+If second argument is absent, "weather" parameter should get value "Great weather!".
+1. First solve this WITHOUT default function parameter.
+2. Comment previous solution and solve same task WITH default function parameter.
+NOTE: Carefully compare your results with test calls results
+*/
+
+// Solution
+
+// Method 1
+
+// const weatherForecast = (city, weather) => {
+//   if (weather === undefined) return `Weather forecast for ${city}: Great weather!`
+//   return `Weather forecast for ${city}: ${weather}`;
+// }
+
+
+// Method 2
+
+const weatherForecast = (city, weather = `Great weather!`) => {
+  return `Weather forecast for ${city}: ${weather}`;
+}
+
+console.log(weatherForecast("Dubai", "Sunny"));
+// Weather forecast for Dubai: Sunny
+
+console.log(weatherForecast("London", "Light rain"));
+// Weather forecast for London: Light rain
+
+console.log(weatherForecast("Paris"));
+// Weather forecast for Paris: Great weather!
+
+console.log(weatherForecast("Miami", ""));
+// Weather forecast for Miami:
+
+console.log(weatherForecast("Las Vegas", undefined));
+// Weather forecast for Las Vegas: Great weather!
+
+
+
 
