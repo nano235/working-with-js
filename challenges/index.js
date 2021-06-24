a38574263d9a9758ba8914e1aa7e7e8943fdd7bf
nano235 <revivaladolor@yahoo.com>
2021-06-24 17:27:27 +0100

    Challenge for array destructuring, tutor's solution

--- challenges/index.js
+++ challenges/index.js
@@ -898,5 +898,49 @@ const qtyRange = [8, 29];
 processQuantities(qtyRange);
 
 
+// CHALLENGE
+
+/*
+Create a function "minMax" that will accept any quantity of the arguments.
+This function should return array of two elements:
+1. First element in the array is minimal value among all arguments
+2. Second element in the array is maximal value among all arguments
+Use destructuring to parse results of the function calls.
+*/
+
+
+let min, max;
+
+//  Solution
+
+const minMax = (...params) => {
+  params.sort((a,b) => a-b)
+  min = params[0]
+  max = params[params.length-1]
+  return (min, max);
+}
+
+
+// Tutor's solution
+
+// const minMax = (...nums) => [
+//   nums.reduce((min, num) => (num < min ? num : min)),
+//   nums.reduce((max, num) => (num > max ? num : max))
+// ];
+
+
+/* call here "minMax" function with arguments 24, 5, 34, 10 */
+minMax(24, 5, 34, 10)
+
+// [min, max] = minMax(24, 5, 34, 10); //tutor's call
+console.log(min, max); // 5, 34
+
+/* call here "minMax" function with arguments 18, 23, 103, 70, 80, 25 */
+minMax(18, 23, 103, 70, 80, 25)
+
+// [min, max] = minMax(18, 23, 103, 70, 80, 25); //Tutor's call
+console.log(min, max); // 18, 103
+
+
 
 
