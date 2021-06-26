6b95635b85dc391f8592ff47965c79b3ac57cd53
nano235 <revivaladolor@yahoo.com>
2021-06-26 08:48:25 +0100

    more challenges for object destructuring

--- challenges/index.js
+++ challenges/index.js
@@ -998,4 +998,59 @@ console.log(mult(obj));
 
 
 
+// CHALLENGE
+
+/*
+Create "processPosts" function that will return new array of posts.
+Notice that some propery names in each post are modified and each postId is incremented by 1000.
+Original array of posts should not be mutated.
+*/
+
+const POSTS = [
+  {
+    postId: 234,
+    author: "robd",
+    commentsQty: 5
+  },
+  {
+    postId: 823,
+    author: "sady"
+  },
+  {
+    postId: 161,
+    author: "merryl",
+    commentsQty: 8
+  }
+];
+
+const processPosts = (posts) => {
+  return posts.map(({postId, author : postAuthor, commentsQty : postCommentsQty = 0}) => 
+  ({postId : postId + 1000, postAuthor, postCommentsQty}))
+};
+
+console.log(processPosts(POSTS));
+/*
+[
+  {
+    postId: 1234,
+    postAuthor: "robd",
+    postCommentsQty: 5
+  },
+  {
+    postId: 1823,
+    postAuthor: "sady",
+    postCommentsQty: 0
+  },
+  {
+    postId: 1161,
+    postAuthor: "merryl",
+    postCommentsQty: 8
+  }
+]
+*/
+
+console.log(POSTS); // original array of posts
+
+
+
 
