96a7dc60d1f5c55385c8b81b0a9545e6fa3b5194
nano235 <revivaladolor@yahoo.com>
2021-06-26 13:30:27 +0100

    more challenges for object destructuring

--- challenges/index.js
+++ challenges/index.js
@@ -1052,5 +1052,38 @@ console.log(processPosts(POSTS));
 console.log(POSTS); // original array of posts
 
 
+// CHALLENGE 
+
+/*
+Use object destructuring with rest object properties to quickly remove specific properties from the object.
+Ensure that variables that will be used for destructuring of the deleted properties will not be accessible 
+after the destructuring operation.
+*/
+
+let PERSON = {
+  _id: "5ad8cefcc0971792dacb3f1f",
+  index: 4,
+  processed: false,
+  cart: ["item1", "item2", "item3"],
+  email: "slarsen@test.com",
+  name: "Samanta Larsen",
+  cartId: 435
+};
+
+{let _id, processed, cart
+
+({_id, processed, cart, ...PERSON} = PERSON)}
+
+console.log(PERSON);
+/*
+{
+  index: 4,
+  email: "slarsen@test.com",
+  name: "Samanta Larsen",
+  cartId: 435
+}
+*/
+
+
 
 
