5a26fb80498cf8d3c6c7a1b51f15ee9a83350aae
nano235 <revivaladolor@yahoo.com>
2021-07-04 23:49:14 +0100

    Challenge; converted function constructor from previous file to class

--- challenges/classes.js
+++ challenges/classes.js
@@ -65,4 +65,137 @@ smallPlane_1.vehicleInfo(); // Max speed of the vehicle is 800 and weight is 15
 largePlane_1.vehicleInfo(); // Max speed of the vehicle is 900 and weight is 40
 
 
+/**
+ * CHALLENGE 7-3: TASK
+ *
+ * Convert all function constructors to ES6 Classes keeping same functionality
+ */
+
+// function Product(props) {
+//     this.price = props.price;
+//   }
+  
+//   Product.prototype.priceInfo = function() {
+//     console.log(`Price of the product is ${this.price}`);
+//   };
+  
+//   function ElectricDevice(props) {
+//     Product.call(this, props);
+//     this.energyEfficiencyClass = props.energyEfficiencyClass;
+//   }
+  
+//   ElectricDevice.prototype = Object.create(Product.prototype);
+  
+//   ElectricDevice.prototype.constructor = ElectricDevice;
+  
+//   ElectricDevice.prototype.energyInfo = function() {
+//     console.log(
+//       `Energy Efficiency Class is ${
+//         this.energyEfficiencyClass
+//       }`
+//     );
+//   };
+  
+//   function TV(props) {
+//     ElectricDevice.call(this, props);
+//     this.model = props.model;
+//     this.diagonal = props.diagonal;
+//   }
+  
+//   TV.prototype = Object.create(ElectricDevice.prototype);
+  
+//   TV.prototype.constructor = TV;
+  
+//   const propsForMyTv = {
+//     model: "A1620",
+//     price: 1200,
+//     energyEfficiencyClass: "A+",
+//     diagonal: 42
+//   };
+  
+//   const myTV = new TV(propsForMyTv);
+  
+//   /**
+//    * VERIFICATION
+//    */
+//   console.log(myTV);
+//   /* {
+//     model: "A1620",
+//     price: 1200,
+//     energyEfficiencyClass: "A+",
+//     diagonal: 42,
+//     __proto__: ...
+//   } */
+  
+//   myTV.energyInfo(); // "Energy Efficiency Class is A+"
+  
+//   myTV.priceInfo(); // Price of the product is 1200
+  
+//   myTV instanceof TV; // true
+//   myTV instanceof ElectricDevice; // true
+//   myTV instanceof Product; // true
+//   myTV instanceof Object; // true
+
+
+// Solution
+
+// Adding "_1" to each class and instances to avoid error of "already declared"
+
+class Product_1 {
+    constructor(props){
+        this.price = props.price;
+    }
+
+    priceInfo(){
+        console.log(`Price of the product is ${this.price}`);
+      };
+}
+
+class ElectricDevice_1 extends Product_1 {
+    constructor(props){
+        super(props)
+        this.energyEfficiencyClass = props.energyEfficiencyClass;
+    }
+
+    energyInfo(){
+        console.log(`Energy Efficiency Class is ${this.energyEfficiencyClass}`);
+      };
+}
+
+class TV_1 extends ElectricDevice_1 {
+    constructor(props){
+        super(props)
+        this.model = props.model;
+        this.diagonal = props.diagonal;
+    }
+}
+
+const propsForMyTv_1 = {
+    model: "A1620",
+    price: 1200,
+    energyEfficiencyClass: "A+",
+    diagonal: 42
+  };
+  
+  const myTV_1 = new TV_1(propsForMyTv_1);
+  
+  /**
+   * VERIFICATION
+   */
+  console.log(myTV_1);
+  /* {
+    model: "A1620",
+    price: 1200,
+    energyEfficiencyClass: "A+",
+    diagonal: 42,
+    __proto__: ...
+  } */
+  
+  myTV_1.energyInfo(); // "Energy Efficiency Class is A+"
+  
+  myTV_1.priceInfo(); // Price of the product is 1200
+
+
+
+
 
