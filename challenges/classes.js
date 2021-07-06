09ab4af49e1c14de4c9ddb06236e15ab0448be35
nano235 <revivaladolor@yahoo.com>
2021-07-06 05:40:51 +0100

    Challenge; creating a custom push method

--- challenges/classes.js
+++ challenges/classes.js
@@ -228,6 +228,44 @@ console.log(newArray_1.sum())
 console.log(newArray_1.onlyNumbers())
 
 
+/**
+ * CHALLENGE 7-5: TASK
+ *
+ * Create new class "CustomArray" that should extend built-in "Array".
+ *
+ * Add one custom method "customPush" to the new class.
+ * This method will have one parameter "newElement".
+ *
+ * It should perform following actions:
+ * 1. Add new element to the existing array
+ * (don't use "push" method for this)
+ * 2. Modify "length" property of the array (increment it)
+ * 3. Log following line to the console:
+ * "New element <ELEMENT> was just added to the array"
+ *
+ * Create instance of the new "CustomArray" class and test new method "customPush" and compare it with "push"
+ *
+ * What will happen if name of the custom method in the "CustomArray" class will be also "push" instead of "customPush"?
+ * Try this.
+ */
+
+//  Solution
+
+
+class CustomArray extends Array {
+    customPush(newElement){
+        this[this.length] = newElement
+        return `New element ${newElement} was just added to the array`
+    }
+}
+
+const newArray_2 = new CustomArray()
+
+
+newArray_2.customPush(1)
+
+
+
 
 
 
